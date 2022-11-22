86a475bfcfcfeb1354018737911dd45b286997fb
Maung N. Islam <MaungNoorIslam@my.uopeople.edu>
2022-11-22 07:52:27 +0600

    Update __about__.py

--- __about__.py
+++ __about__.py
@@ -1,8 +1,8 @@
 import time
 
 __version__ = "0.5.2"
-__author__ = "Lukas Hedegaard"
-__author_email__ = "lukasxhedegaard@gmail.com"
+__author__ = "Maung Noor Islam"
+__author_email__ = "MaungNoor.Islam@studentambassador.com"
 __license__ = "Apache-2.0"
 __copyright__ = f'Copyright (c) 2022-{time.strftime("%Y")}, {__author__}'
 __homepage__ = "https://github.com/lukashedegaard/structured-pruning-adapters"
